9800c97772ea63265455f8d12b7259d65f0c965a
김수민 <mytoutbon@gmail.com>
2019-11-24 21:21:02 +0900

    Correct typos, Review django_myform class

--- articles/views.py
+++ articles/views.py
@@ -10,33 +10,31 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 
-
 # GET 으로 들어오면? 생성하는 페이지 rendering
 # POST 로 들어오면? 생성하는 로직 수행
 def create(request):
     if request.method == 'POST':
-        # 사용자의 모든 입력 form 통해 받는다. 
+        # 사용자의 모든 입력 form 통해 받는다.
         form = ArticleForm(request.POST)
-
-        # 사용자가 입력한(보낸) 데이터가 유효한지 확인
         # 처음 함수처럼 Raw Data 를 바로 꺼내는 것은 위험
         # title = request.POST.get('title') (위험)
-        # content = request.POST.get('content') (위험) 그대신...       
+        # content = request.POST.get('content') (위험)
+
+        # 사용자가 입력한(보낸) 데이터가 유효한지 확인
         if form.is_valid():
             title = form.cleaned_data.get('title')
             content = form.cleaned_data.get('content')
             article = Article(title=title, content=content)
             article.save()
             return redirect('articles:index')
+        # form.is_valid() 가 False 인 경우
         # valid 하지 않은 값 들어있는 form 을 그대로 담아서 사용자에게 다시 보냄
-        # 수정해서 다시 입력하라고! 
+        # 수정해서 다시 입력하라고!
         # else:
         #     context = {'form': form}
         #     return render(request, 'articles/create.html', context)
 
-
-    else: # GET 요청으로 들어왔을 때
-        # form
+    else:  # GET 요청으로 들어왔을 때
         form = ArticleForm()
 
     # if ~ else~ 동시에 걸리게 indent 앞으로 조정
@@ -44,9 +42,8 @@ def create(request):
     return render(request, 'articles/create.html', context)
 
 
-
-def detail(request, article_pk): # variable routing 이라 두번재 인자 존재
-    article = get_object_or_404(Article, pk=article_pk) # 그 번호에 맞는 article 1개만 꺼내서 저장
-    context = {'article': article} # 넘김
+def detail(request, article_pk):  # variable routing 이라 두번째 인자 존재
+    # 그 번호에 맞는 article 1개만 꺼내서 저장
+    article = get_object_or_404(Article, pk=article_pk)
+    context = {'article': article}  # 넘김
     return render(request, 'articles/detail.html', context)
-
